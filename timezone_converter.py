13c81d37890c477c2189cd2e0babc7cf2ffdfe90
Vinícius Oviedo <65742412+OviedoVR@users.noreply.github.com>
2023-04-08 11:03:01 -0300

    Update timezone_converter.py

--- timezone_converter.py
+++ timezone_converter.py
@@ -81,8 +81,7 @@ st.set_page_config(
         time zone in UTC format  and have its correspondent result 
         from a user-entered PST time.
         """
-    }  
-
+    }
 )
 st.header('Time Zone Coverter Streamlit app')
 
